0f4a5e15e60c2effe8f0452f642338a8d4eb1c67
Janis Zisenis <janis.zisenis@netlight.com>
2024-09-07 18:12:50 +0200

    Run scripts as executables
    (directly without specifying the shell)

--- app/core/scripts.go
+++ app/core/scripts.go
@@ -5,16 +5,9 @@ import (
 	"os/exec"
 )
 
-var bash = "/bin/bash"
-
 func ExecuteScript(file string, args []string) error {
-	bashArgs := append([]string{file}, args...)
-	script := exec.Command(bash, bashArgs...)
-
-	//script.Stdin = os.Stdin
+	script := exec.Command(file, args...)
 	script.Stdout = os.Stdout
-	//script.Stderr = os.Stderr
-
 	err := script.Run()
 
 	return err
