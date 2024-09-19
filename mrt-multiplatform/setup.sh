a7b39ff096c97da96615c94b7b3ccdbeb652b277
Janis Zisenis <janis.zisenis@netlight.com>
2024-09-19 21:15:08 +0200

    Install new package version of mrt multiplatform

--- mrt-multiplatform/setup.sh
+++ mrt-multiplatform/setup.sh
@@ -4,4 +4,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 # shellcheck source=./detection-functions.sh
 . "$SCRIPT_DIR/detection-functions.sh"
 
-ln -s "$SCRIPT_DIR/mrt-$(detect_os)-$(detect_arch)" "$SCRIPT_DIR/../mrt"
+cp "$SCRIPT_DIR/mrt-$(detect_os)-$(detect_arch)" "$SCRIPT_DIR/../mrt"
+
+#ln -s "$SCRIPT_DIR/mrt-$(detect_os)-$(detect_arch)" "$SCRIPT_DIR/../mrt"
