2d4205e34b8a2d7d6609b71d36274b82f6d9d784
Janis Zisenis <janis.zisenis@netlight.com>
2024-09-20 09:32:42 +0200

    add .exe to windows executable in setup script

--- mrt-multiplatform/setup.sh
+++ mrt-multiplatform/setup.sh
@@ -4,6 +4,13 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 # shellcheck source=./detection-functions.sh
 . "$SCRIPT_DIR/detection-functions.sh"
 
-cp "$SCRIPT_DIR/mrt-$(detect_os)-$(detect_arch)" "$SCRIPT_DIR/../mrt"
+OS=$(detect_os)
 
-#ln -s "$SCRIPT_DIR/mrt-$(detect_os)-$(detect_arch)" "$SCRIPT_DIR/../mrt"
+sourceName="$SCRIPT_DIR/mrt-$OS-$(detect_arch)"
+
+if [[ OS == "windows" ]]
+then
+    sourceName+=".exe"
+fi
+
+cp "$SCRIPT_DIR/mrt-$(detect_os)-$(detect_arch)" "$SCRIPT_DIR/../mrt"
