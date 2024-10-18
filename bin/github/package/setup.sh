1d5b64c8f14baf83403b8be575dccbf53be16c4d
Janis Zisenis <janis.zisenis@netlight.com>
2024-10-18 21:35:24 +0200

    Updates binaries for github

--- bin/github/package/setup.sh
+++ bin/github/package/setup.sh
@@ -4,4 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 # shellcheck source=./detection-functions.sh
 . "$SCRIPT_DIR/detection-functions.sh"
 
-cp "$SCRIPT_DIR/mrt-$(detect_os)-$(detect_arch)" "$SCRIPT_DIR/../mrt"
+os=$(detect_os)
+arch=$(detect_arch)
+
+cp "$SCRIPT_DIR/mrt-$os-$arch" "$SCRIPT_DIR/../mrt"
