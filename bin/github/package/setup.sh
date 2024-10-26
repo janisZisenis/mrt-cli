94aeb81dd889de56007f464848871781c149dafd
Janis Zisenis <janis.zisenis@netlight.com>
2024-10-26 11:58:55 +0200

    Delete github binaries from repository
    The action is now downloading the first release itself

--- bin/github/package/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-# shellcheck source=./detection-functions.sh
-. "$SCRIPT_DIR/detection-functions.sh"
-
-cp "$SCRIPT_DIR/mrt-$(detect_os)-$(detect_arch)" "$SCRIPT_DIR/../mrt"
