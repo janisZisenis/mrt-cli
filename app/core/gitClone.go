34d297170273e384505c1f1bf36df496f691d8bb
Janis Zisenis <janis.zisenis@netlight.com>
2025-06-21 15:19:19 +0200

    Split up gitClone and gitBranch

--- app/core/gitClone.go
+++ app/core/gitClone.go
@@ -2,36 +2,12 @@ package core
 
 import (
 	"app/log"
-	"bytes"
-	"errors"
 	"fmt"
 	"io"
 	"os"
 	"os/exec"
-	"strings"
 	"sync"
 )
-
-func GetCurrentBranchShortName(repoDir string) (string, error) {
-	cmd := exec.Command("git", "-C", repoDir, "rev-parse", "--abbrev-ref", "HEAD")
-
-	var stdout bytes.Buffer
-	cmd.Stdout = &stdout
-
-	err := cmd.Run()
-
-	if err != nil {
-		log.Error("The given path \"" + repoDir + "\" does not contain a repository.")
-		os.Exit(1)
-	}
-
-	branchName := strings.TrimSpace(stdout.String())
-	if branchName == "" {
-		return "", errors.New("could not determine the current branch: output was empty")
-	}
-
-	return branchName, nil
-}
 
 func CloneRepository(repositoryUrl, destination string) {
 	log.Info("Cloning " + repositoryUrl)
