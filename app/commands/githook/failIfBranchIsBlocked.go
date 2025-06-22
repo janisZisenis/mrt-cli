f4158d055813a0548f17a8d93f852e686ae1a20f
Janis Zisenis <janis.zisenis@netlight.com>
2025-06-22 09:53:20 +0200

    Reuse log.Error in failIfBranchIsBlocked

--- app/commands/githook/failIfBranchIsBlocked.go
+++ app/commands/githook/failIfBranchIsBlocked.go
@@ -2,14 +2,14 @@ package githook
 
 import (
 	"app/core"
-	"fmt"
+	"app/log"
 	"os"
 	"slices"
 )
 
 func failIfBranchIsBlocked(teamInfo core.TeamInfo, branch string, action string) {
 	if slices.Contains(teamInfo.BlockedBranches, branch) {
-		fmt.Println("Action \"" + action + "\" not allowed on branch \"" + branch + "\"")
+		log.Error("Action \"%s\" not allowed on branch \"%s\"", action, branch)
 		os.Exit(1)
 	}
 }
