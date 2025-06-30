410a46dda361ae677a3aefa7723cfdc8a2c764e3
Janis Zisenis <janis.zisenis@netlight.com>
2025-06-30 22:40:08 +0200

    add customizable shortDescription for run commands

--- app/commands/run/runscript/command.go
+++ app/commands/run/runscript/command.go
@@ -2,17 +2,67 @@ package runscript
 
 import (
 	"app/core"
+	"app/log"
+	"errors"
+	"github.com/spf13/viper"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
 
-var ScriptsPath = "/run/*/" + core.CommandFileName()
+var runDirectory = "/run"
+var ScriptsPath = runDirectory + "/*/" + core.CommandFileName()
+
+type CommandConfig struct {
+	ShortDescription string `json:"shortDescription"`
+}
+
+func LoadCommandConfig(commandPath string) CommandConfig {
+	commandDir := filepath.Dir(commandPath)
+	setupDefaults(commandDir)
+
+	var config CommandConfig
+
+	viper.AddConfigPath(commandDir)
+	configFileName := "config"
+	configFileExtension := "json"
+	viper.SetConfigName(configFileName)
+	viper.SetConfigType(configFileExtension)
+
+	readErr := viper.ReadInConfig()
+	if readErr != nil {
+		if errors.As(readErr, &viper.ConfigFileNotFoundError{}) {
+			return defaultConfig(commandDir)
+		}
+
+		log.Errorf("Error while reading %s/%s.%s", commandDir, configFileName, configFileExtension)
+		log.Errorf("%v", readErr)
+		os.Exit(1)
+	}
+
+	_ = viper.Unmarshal(&config)
+
+	return config
+}
+
+func setupDefaults(commandDir string) {
+	viper.SetDefault("shortDescription", defaultConfig(commandDir).ShortDescription)
+}
+
+func defaultConfig(commandDir string) CommandConfig {
+	description := "Executes run command " + filepath.Base(commandDir)
+	return CommandConfig{
+		ShortDescription: description,
+	}
+}
 
 func MakeCommand(scriptName string, scriptPath string) *cobra.Command {
+	config := LoadCommandConfig(scriptPath)
+
 	var command = &cobra.Command{
 		Use:   scriptName,
-		Short: "Executes run command " + scriptName,
+		Short: config.ShortDescription,
 		Run: func(_ *cobra.Command, args []string) {
 			command(scriptPath, args)
 		},
--- e2e-tests/tests/run-commands/run-command-config-test.bats
+++ e2e-tests/tests/run-commands/run-command-config-test.bats
@@ -9,18 +9,47 @@ teardown() {
 	common_teardown
 }
 
-@test "if command succeeds with output it will print the command's output" {
+@test "if command config contains shortDescription, it is displayed in help" {
 	commandName="some-command"
   commandLocation="$(testEnvDir)/run"
   shortDescription="A command that outputs some-output"
   writeStubCommand "$commandLocation" "$commandName" "0" "some-output"
   cat <<EOF >"$(testEnvDir)/run/$commandName/config.json"
 {
-  "short_description": "$shortDescription"
+  "shortDescription": "$shortDescription"
 }
 EOF
 
   run bats_pipe runCommand "-h" \| grep "$commandName"
 
 	assert_output "  $commandName $shortDescription"
+}
+
+@test "if command config does not contain shortDescription the default is displayed in help" {
+	commandName="some-command"
+  commandLocation="$(testEnvDir)/run"
+  shortDescription="A command that outputs some-output"
+  writeStubCommand "$commandLocation" "$commandName" "0" "some-output"
+  cat <<EOF >"$(testEnvDir)/run/$commandName/config.json"
+{}
+EOF
+
+  run bats_pipe runCommand "-h" \| grep "$commandName"
+
+	assert_output "  $commandName Executes run command $commandName"
+}
+
+@test "if command config is completely empty, it should exit with an error" {
+	commandName="some-command"
+  commandLocation="$(testEnvDir)/run"
+  shortDescription="A command that outputs some-output"
+  configFile="$(testEnvDir)/run/$commandName/config.json"
+  writeStubCommand "$commandLocation" "$commandName" "0" "some-output"
+  touch "$configFile"
+
+  run runCommand "-h"
+
+  assert_equal "$status" 1
+  assert_line --index 0 "Error while reading $configFile"
+  assert_line --index 1 "While parsing config: unexpected end of JSON input"
 }
