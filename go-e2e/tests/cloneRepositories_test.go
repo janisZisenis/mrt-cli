61d8a440bc68de716eed951ad98387174c8b50b6
Test User <test@example.com>
2025-11-09 13:22:08 +0100

    Move lines

--- go-e2e/tests/cloneRepositories_test.go
+++ go-e2e/tests/cloneRepositories_test.go
@@ -16,17 +16,10 @@ import (
 func TestCloneRepositoriesToCustomPath(t *testing.T) {
 	t.Parallel()
 	repositoryDir := "/Users/jazi/Documents/Development/mrt/repositories/mrt-cli"
+
 	agent := fixtures.AuthenticatedFixture(t, repositoryDir+"/.ssh/private-key")
 	_ = setupPath(repositoryDir)
-
 	_ = agent.ShowKeys()
-	tempDir := t.TempDir()
-	teamConfig := tempDir + "/team.json"
-	repositoryName := "1_TestRepository"
-	_ = utils.WriteJSONFile(teamConfig, map[string]interface{}{
-		"repositories": []string{"git@github-testing:janisZisenisTesting/" + repositoryName + ".git"},
-	})
-
 	mrtCmd := exec.Command("mrt", "--team-dir", repositoryDir, "run", "binary-location", "--", "--exe-name")
 	exePathBytes, err := mrtCmd.Output()
 	if err != nil {
@@ -34,6 +27,13 @@ func TestCloneRepositoriesToCustomPath(t *testing.T) {
 		os.Exit(1)
 	}
 	exePath := stringTrimNewline(string(exePathBytes))
+	tempDir := t.TempDir()
+
+	teamConfig := tempDir + "/team.json"
+	repositoryName := "1_TestRepository"
+	_ = utils.WriteJSONFile(teamConfig, map[string]interface{}{
+		"repositories": []string{"git@github-testing:janisZisenisTesting/" + repositoryName + ".git"},
+	})
 
 	cmd := exec.Command(exePath, "--team-dir", tempDir, "setup", "clone-repositories")
 	cmd.Env = append(os.Environ(), agent.Env()...)
