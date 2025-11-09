f3e3b5bbcde5c94009eb2ccd9e8ae7781bd09046
Test User <test@example.com>
2025-11-09 14:19:27 +0100

    Extract test main function

--- go-e2e/tests/cloneRepositories_test.go
+++ go-e2e/tests/cloneRepositories_test.go
@@ -1,39 +1,36 @@
 package tests
 
 import (
-	"bytes"
-	"fmt"
 	"mrt-cli/go-e2e/assertions"
 	"mrt-cli/go-e2e/fixtures"
-
 	"mrt-cli/go-e2e/utils"
 	"os"
 	"os/exec"
-	"strings"
 	"testing"
 )
 
 func TestCloneRepositoriesToCustomPath(t *testing.T) {
 	t.Parallel()
 	repositoryDir := "/Users/jazi/Documents/Development/mrt/repositories/mrt-cli"
-
 	agent := fixtures.AuthenticatedFixture(t, repositoryDir+"/.ssh/private-key")
-	_ = setupPath(repositoryDir)
-	_ = agent.ShowKeys()
-	mrtCmd := exec.Command("mrt", "--team-dir", repositoryDir, "run", "binary-location", "--", "--exe-name")
-	exePathBytes, err := mrtCmd.Output()
-	if err != nil {
-		t.Fatalf("failed to get executable path: %v", err)
-	}
-	exePath := stringTrimNewline(string(exePathBytes))
 	tempDir := t.TempDir()
+	repositoryName := writeTeamConfig(tempDir)
 
+	cloneRepositories(t, tempDir, agent)
+
+	assertions.TestDirectoryExists(t, tempDir+"/repositories/"+repositoryName+"/.git")
+}
+
+func writeTeamConfig(tempDir string) string {
 	repositoryName := "1_TestRepository"
 	_ = utils.TeamConfigWriter(tempDir, map[string]interface{}{
 		"repositories": []string{"git@github-testing:janisZisenisTesting/" + repositoryName + ".git"},
 	})
+	return repositoryName
+}
 
-	cmd := exec.Command(exePath, "--team-dir", tempDir, "setup", "clone-repositories")
+func cloneRepositories(t *testing.T, tempDir string, agent *utils.Agent) {
+	cmd := exec.Command(binaryName, "--team-dir", tempDir, "setup", "clone-repositories")
 	cmd.Env = append(os.Environ(), agent.Env()...)
 
 	cmd.Stdout = os.Stdout
@@ -42,39 +39,4 @@ func TestCloneRepositoriesToCustomPath(t *testing.T) {
 	if err := cmd.Run(); err != nil {
 		t.Fatalf("command failed: %v", err)
 	}
-
-	assertions.TestDirectoryExists(t, tempDir+"/repositories/"+repositoryName+"/.git")
-}
-
-func stringTrimNewline(s string) string {
-	if len(s) > 0 && s[len(s)-1] == '\n' {
-		return s[:len(s)-1]
-	}
-	return s
-}
-
-func setupPath(repositoryDir string) error {
-	cmd := exec.Command("mrt", "--team-dir", repositoryDir, "run", "binary-location", "--", "--dir")
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	cmd.Stderr = os.Stderr
-
-	if err := cmd.Run(); err != nil {
-		return fmt.Errorf("failed to run mrt command: %w", err)
-	}
-
-	dir := strings.TrimSpace(out.String())
-
-	if dir == "" {
-		return fmt.Errorf("command returned empty directory")
-	}
-
-	currentPath := os.Getenv("PATH")
-	newPath := currentPath + string(os.PathListSeparator) + dir
-
-	if err := os.Setenv("PATH", newPath); err != nil {
-		return fmt.Errorf("failed to update PATH: %w", err)
-	}
-
-	return nil
 }
---
+++ go-e2e/tests/main_test.go
@@ -0,0 +1,77 @@
+package tests
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+var repositoryDir = "/Users/jazi/Documents/Development/mrt/repositories/mrt-cli"
+var binaryName string
+
+func TestMain(m *testing.M) {
+	binaryDir := getBinaryDir(repositoryDir)
+	binaryName = getBinaryName()
+
+	panicIfBinaryCanNotBeFound(binaryDir, binaryName)
+
+	addToPath(binaryDir)
+
+	os.Exit(m.Run())
+}
+
+func panicIfBinaryCanNotBeFound(binaryDir string, binaryName string) {
+	binaryPath := binaryDir + "/" + binaryName
+	if _, err := os.Stat(binaryPath); os.IsNotExist(err) {
+		panic("binary not found at: " + binaryPath)
+	}
+}
+
+func addToPath(binaryDir string) {
+	currentPath := os.Getenv("PATH")
+	newPath := currentPath + string(os.PathListSeparator) + binaryDir
+
+	if err := os.Setenv("PATH", newPath); err != nil {
+		panic("failed to update add binaryDir to PATH: " + binaryDir)
+	}
+}
+
+func getBinaryDir(repositoryDir string) string {
+	cmd := exec.Command("mrt", "--team-dir", repositoryDir, "run", "binary-location", "--", "--dir")
+	binaryDirBytes, err := cmd.Output()
+
+	output := string(binaryDirBytes)
+
+	if err != nil {
+		panic("failed to run mrt command: " + output)
+	}
+
+	binaryDir := strings.TrimSpace(output)
+
+	if binaryDir == "" {
+		panic("command returned empty directory")
+	}
+
+	return binaryDir
+}
+
+func getBinaryName() string {
+	cmd := exec.Command("mrt", "--team-dir", repositoryDir, "run", "binary-location", "--", "--exe-name")
+	exePathBytes, err := cmd.Output()
+
+	output := string(exePathBytes)
+
+	if err != nil {
+		panic("failed to get executable path: " + output)
+	}
+
+	return stringTrimNewline(output)
+}
+
+func stringTrimNewline(s string) string {
+	if len(s) > 0 && s[len(s)-1] == '\n' {
+		return s[:len(s)-1]
+	}
+	return s
+}
