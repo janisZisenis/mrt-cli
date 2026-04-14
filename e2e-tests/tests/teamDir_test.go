10fc78a5a446e492c331c8929f6b1449c74a7d61
Test User <test@example.com>
2026-04-14 17:40:31 +0200

    Add a test for checking if the passed team-dir actually exists

---
+++ e2e-tests/tests/teamDir_test.go
@@ -0,0 +1,21 @@
+package tests_test
+
+import (
+	"mrt-cli/e2e-tests/fixtures"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_IfTeamDirDoesNotExist_ShouldPrintErrorMessageAndExitWithError(t *testing.T) {
+	f := fixtures.MakeMrtFixture(t).Parallel()
+
+	output, exitCode := f.MakeMrtCommand().
+		RunInDirectory("/some/unknown/path").
+		Setup().
+		All().
+		Execute()
+
+	assert.NotEqual(t, 0, exitCode)
+	output.AssertHasLine(t, "Team directory \"/some/unknown/path\" does not exist.")
+}
